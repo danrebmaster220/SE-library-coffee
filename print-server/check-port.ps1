6b3f26df80cae72e56c68b8f635115f9845b5dc4
danrebmaster220 <111111111111111@gmail.com>
2026-03-07 14:53:55 +0800

    chore: remove diagnostic and unused files from print-server
    Deleted all .ps1, .js, .txt, and package.json files used for debugging. Only PrintServer.ps1 and START_PRINT_SERVER.bat remain.

--- print-server/check-port.ps1
+++
@@ -1,3 +0,0 @@
-Get-PrinterPort | Where-Object { $_.Name -like 'USB*' } | Format-Table Name, Description -AutoSize
-Write-Host ""
-Get-Printer -Name "POS-58" -ErrorAction SilentlyContinue | Format-List Name, PortName, DriverName, PrinterStatus
